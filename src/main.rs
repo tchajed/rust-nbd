d5183ba9d43a095ad3a2ee858b83e45c5b048f80
Tej Chajed <tchajed@mit.edu>
2022-06-07 15:20:07 -0400

    Set file size natively

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,14 @@
-use std::{
-    fs::{File, OpenOptions},
-    io::Write,
-};
+use std::fs::OpenOptions;
 
 use nbd::{Export, Server};
 
-fn set_size(f: &mut File, size: usize) -> std::io::Result<()> {
-    let meta = f.metadata()?;
-    if meta.len() == 0 {
-        let write_size = 4096;
-        let mut buf = vec![0; write_size];
-        for _ in 0..(size / write_size) {
-            f.write_all(&mut buf)?;
-        }
-        let bytes_written = (size / write_size) * write_size;
-        f.write_all(&buf[..(size - bytes_written)])?;
-    }
-    Ok(())
-}
-
 fn main() -> std::io::Result<()> {
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open("disk.img")?;
-    set_size(&mut file, 10 * 1024 * 1024)?;
+    file.set_len(10 * 1024 * 1024)?;
     let export = Export {
         name: "default".to_string(),
         file,
