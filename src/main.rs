aa24449cc56a5c6afef9a46532ee9b10b6de220d
Tej Chajed <tchajed@mit.edu>
2022-06-07 21:36:25 -0400

    Add a CLI

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,45 @@
+use clap::Parser;
 use color_eyre::Result;
 use std::fs::OpenOptions;
 
 use nbd::{Export, Server};
 
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+struct Args {
+    #[clap(long)]
+    no_create: bool,
+
+    #[clap(long, default_value = "default")]
+    export: String,
+
+    #[clap(short, long, default_value_t = 10)]
+    size: usize,
+
+    #[clap(default_value = "disk.img")]
+    filename: String,
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
     env_logger::init();
 
+    let args = Args::parse();
+    let create = !args.no_create;
+    let size_bytes = args.size as u64 * 1024 * 1024;
+
     let file = OpenOptions::new()
         .read(true)
         .write(true)
-        .create(true)
-        .open("disk.img")?;
-    file.set_len(10 * 1024 * 1024)?;
+        .create(create)
+        .open(args.filename)?;
+
+    file.set_len(size_bytes)?;
+
     let export = Export {
-        name: "default".to_string(),
+        name: args.export,
         file,
     };
-    let server = Server::new(export);
-    server.start()?;
+    Server::new(export).start()?;
     Ok(())
 }
